b897b7650842c057c2ca21c8e0116ef399b8aff6
Tosa <eea85670@gmail.com>
2025-04-15 21:35:32 +0400

    Added GLFW and GLAD

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 
-int main()
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
 {
-	std::cout << "Hello, World!" << std::endl;
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    int version = gladLoadGL();
+    if (version == 0) {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
+    }
+
+    glClearColor(0.2f, 0.5f, 0.1f, 1.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
