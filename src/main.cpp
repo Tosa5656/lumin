3b9a2ce0770bee43f17a7afe584151be8ad13935
Tosa5656 <eea85670@gmail.com>
2026-02-26 22:42:45 +0400

    glfw, glad, glm

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
+#include <lumin.h>
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    int version = gladLoadGL(glfwGetProcAddress);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glClearColor(0.5f, 0.1f, 0.1f, 1.0f);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
